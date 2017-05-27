0d8284cbe5a32fb1a8a044a716a645ed2a95ab5f
Pete Wingard <petewingard@gmail.com>
2017-05-27 11:15:21 -0400

    cleaned up files

--- MathClass/example.php
+++ MathClass/example.php
@@ -32,24 +32,7 @@ class TestMathClass{
         return $Math;
     }
 
-//    public function testMathClass(){
-//
-//        $Math = new MathClass($this->var1,$this->var2,$this->var3);
-//
-//        if($Math->isNumeric()){
-//            $Math->mul()->grp(2)
-//        }
-//
-//
-//            ($Math->result) ?                       //math operator
-//                   //post-operator
-//                : false
-//            ) : false;
-//        if(!$is_no_error) {
-//            $Math->result = "Input error.";
-//        }
-//        return $Math;
-//    }
+
 }
 
 
