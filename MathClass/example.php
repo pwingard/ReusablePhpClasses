c9418710b670fb065de07a08fe3d0505854c2f8e
Pete Wingard <petewingard@gmail.com>
2017-05-27 12:33:48 -0400

    now with two example types, ternary for brevity and if/then for readability

--- MathClass/example.php
+++ MathClass/example.php
@@ -6,30 +6,32 @@ require_once "MathClass.php";
 $mymy = new TestMathClass;
 
 echo $mymy->testMathClassTernary()->result.PHP_EOL;
+echo $mymy->testMathClass()->result.PHP_EOL;
 
 
 class TestMathClass{
 
-    public $var1 = 1;
+    public $var1 = 8;
     public $var2 = 2;
     public $var3 = 3;
 
     public function testMathClassTernary(){
-
         $Math = new MathClass($this);
-        $is_no_error =
-                    ($Math->isNumeric()) ? (                //pre-operator
-                        ($Math->result) ?                       //math operator
-                            $Math->add()->grp(2)    //post-operator
-                            : false
-                    ) : false;
-        if(!$is_no_error) {
-            $Math->result = "Input error.";
-        }
+        ($Math->isNumeric()) ? (($Math->result) ? $Math->add()->grp(2) : $Math->result = "Input error." ) : $Math->result = "Input error.";
         return $Math;
     }
 
 
+    public function testMathClass(){
+
+        $Math = new MathClass($this);
+
+        if($Math->isNumeric()){                 //pre-operator
+            $Math->mul()                        //math operator
+                ->grp(2);                   //post-operator
+        } else{
+            $Math->result = "Input error.";
+        }
+        return $Math;
+    }
 }
-
-
