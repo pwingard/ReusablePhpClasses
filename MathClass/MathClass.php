64f52bd633e4ed0702611f5b1a00586c0a10e413
Pete Wingard <petewingard@gmail.com>
2017-05-27 11:17:34 -0400

    added namespaces

--- MathClass/MathClass.php
+++ MathClass/MathClass.php
@@ -1,5 +1,5 @@
 <?php
-
+namespace reusableclasses\mathclass;
 
 /**
  * Class MathClass
--- MathClass/example.php
+++ MathClass/example.php
@@ -1,8 +1,5 @@
 <?php
-
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+namespace reusableclasses\mathclass;
 
 require_once "MathClass.php";
 
